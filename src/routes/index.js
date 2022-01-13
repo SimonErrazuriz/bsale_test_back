a6a63db730edd4d685384edb2b150e2c8c543d93
Simón Errázuriz <simonerrazurizrubilar@gmail.com>
2022-01-12 23:15:33 -0300

    feat: implementar buscador

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,14 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
-router.get('/productos', async (req, res) => {
-    const productos = await pool.query('SELECT * FROM product');
-    res.send(productos);
+router.get(`/productos/:buscar`, async (req, res) => {
+    if (req.params.buscar === 'null') {
+        const productos = await pool.query(`SELECT * FROM product`);
+        res.send(productos);
+    } else {
+        const productos = await pool.query(`SELECT * FROM product WHERE name like '%${req.params.buscar}%'`);
+        res.send(productos);
+    }
 });
 
 module.exports = router;
